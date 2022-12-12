08cb620effde26d8f64b25630b8ce7e62530f517
Mahendra <mahendravpatil3@gmail.com>
2022-12-12 10:36:38 +0530

    logo update

--- src/components/todo.js
+++ src/components/todo.js
@@ -73,7 +73,7 @@ const Todo = () => {
             <div className='child-div'>
                 <figure>
                     {/* <img src="./componets/image/download.png" alt="todo logo"></img> */}
-                    <figcaption>Add your list here✌</figcaption>
+                    {/* <figcaption>Add your list here✌</figcaption> */}
                 </figure>
                 <div className='addItems'>
                     <input type="text" placeholder='✍add Item' className='form-control' value={inputData} onChange={(event) => setInputData(event.target.value) }/>
